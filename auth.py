b8b7bf664b2d486e3d86f49032528104c88a3de9
Artem Kucherskyi <a.kucherskyy@gmail.com>
2015-11-23 16:54:48 +0200

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,4 @@
+authsecr = "4A4sePR2HorfqT5DXTph7NOrqzqkU0kQbeaUcrTs4Nh2KI6aOS"
+authkey = "Y2W7ofIwUhV8X6c9YnASKoTHZ"
+tokenkey = '2330051484-z3UMoyMg3BoEQMsVQeCzVyzdyhbgamwuXYEff0O'
+tokensecret = 'gkglDuRt1fOBAxpH6jQB91qnH9E6QdyXDQRLTQ9m8deLi'
