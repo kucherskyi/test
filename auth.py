258b7025caeab688f64f70d27fe3d9530d644959
Artem Kucherskyi <a.kucherskyy@gmail.com>
2015-11-25 15:41:54 +0200

    Fixed

--- auth.py
+++ auth.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 authsecr = "4A4sePR2HorfqT5DXTph7NOrqzqkU0kQbeaUcrTs4Nh2KI6aOS"
 authkey = "Y2W7ofIwUhV8X6c9YnASKoTHZ"
 tokenkey = '2330051484-z3UMoyMg3BoEQMsVQeCzVyzdyhbgamwuXYEff0O'
